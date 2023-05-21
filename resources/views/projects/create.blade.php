7e08d37b220472977422846ab4102adfc0da0165
noaman jebreel <m.jebreel22@gmail.com>
2023-05-21 14:49:02 +0300

    first commit

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,27 @@
+{{-- Form without any design --}}
+
+{{-- TASK: add the validation errors here - with whatever HTML structure you want --}}
+{{-- in case of title/description empty, visitor should see --}}
+{{-- "The name field is required." and "The description field is required." --}}
+
+<form method="POST" action="{{ route('projects.store') }}">
+    @csrf
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    Title:
+    <br />
+    <input type="text" name="title" />
+    <br /><br />
+    Description:
+    <br />
+    <input type="text" name="description" />
+    <br /><br />
+    <button type="submit">Save</button>
+</form>
